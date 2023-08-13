a53a997e0169fca97d85d680ddb51d8e7d8afcd1
Joe <josephkinyoda@Joes-MacBook-Pro.local>
2023-08-13 22:08:15 +0300

    Added Market Open Hours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 import csv
-from datetime import datetime
+from datetime import datetime, timedelta, timezone
 import smtplib
 from email.message import EmailMessage
 
@@ -15,6 +15,26 @@ SENDER_EMAIL = 'your_email@gmail.com'   # replace with your email
 SENDER_PASSWORD = 'your_password'      # replace with your password or use an app-specific password
 RECIPIENT_EMAIL = 'recipient_email@gmail.com'  # replace with the recipient's email
 
+
+def is_forex_market_open():
+    # Define the Eastern Time Zone (ET)
+    ET = timezone(timedelta(hours=-5))  # For Eastern Standard Time. Adjust for Daylight Saving Time if needed.
+
+    # Get the current time in ET
+    current_time_et = datetime.now(ET)
+
+    # Market opens at 5:00 pm ET on Sunday and closes at 5:00 pm ET on Friday
+    market_open_time = current_time_et.replace(hour=17, minute=0, second=0, microsecond=0)
+    
+    # Check if current time is within the forex market hours
+    if current_time_et.weekday() == 6 and current_time_et < market_open_time:
+        return False  # Sunday before 5:00 pm ET
+    if current_time_et.weekday() == 4 and current_time_et >= market_open_time:
+        return False  # Friday after 5:00 pm ET
+    if current_time_et.weekday() == 5:
+        return False  # All of Saturday
+
+    return True  # Market is open
 
 def fetch_data(session):
     symbols = None
@@ -72,13 +92,16 @@ def send_email(error_message):
 
 
 if __name__ == "__main__":
-    try:
-        fetched_symbols = fetch_data(session)
-        if fetched_symbols:
-            append_to_csv(fetched_symbols)
-            print(f"Data appended to {OUTPUT_FILE}")
-        else:
-            print("Failed to fetch symbols")
-    except Exception as e:
-        send_email(f"Unexpected error in script: {e}")
-        print(f"Unexpected error: {e}")
+    if not is_forex_market_open():
+        print("Forex market is closed. Script will not run.")
+    else:
+        try:
+            fetched_symbols = fetch_data(session)
+            if fetched_symbols:
+                append_to_csv(fetched_symbols)
+                print(f"Data appended to {OUTPUT_FILE}")
+            else:
+                print("Failed to fetch symbols")
+        except Exception as e:
+            send_email(f"Unexpected error in script: {e}")
+            print(f"Unexpected error: {e}")
